8d28505773e5a2efd1be4a50bcced8e7c29daae0
Kirill <makfa78@gmail.com>
2018-07-04 22:35:20 +0300

    All

---
+++ BSA4/BSA4/Comment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA4
+{
+    class Comment
+    {
+        private int id;
+        private DateTime createdAt;
+        private string body;
+        private int postId;
+        private int userId;
+        private int likes;
+      
+        public int PostId { get { return postId; } }
+        public int Id { get { return id; } }
+        public DateTime CreatedAt { get { return createdAt; } }
+
+        public int UserId { get { return userId; } }
+
+        public string Body { get { return body; } }
+        public int Likes { get { return likes; } }
+
+        public Comment(int id, DateTime createdAt, string body, int postId, int userId, int likes)
+        {
+            this.id = id;
+            this.createdAt = createdAt;
+            this.body = body;
+            this.postId = postId;
+            this.userId = userId;
+            this.likes = likes;
+        }
+
+
+        public override string ToString()
+        {
+            return "----This comments has id: " + id + " createad at: " + createdAt + " by: " + userId + " under the post: " + postId + ". It says: " + body + " and have" + likes + " likes";
+        }
+    }
+}
---
+++ BSA4/BSA4/Todo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA4
+{
+    class Todo
+    {
+        private int id;
+        private DateTime createdAt;
+        private string name;
+        private bool isComplete;
+        private int userId;
+
+
+        public int UserId { get { return userId; } }
+        public int Id { get { return id; } }
+        public DateTime CreatedAt { get { return createdAt; } }
+        public bool ISComplete { get { return isComplete; } }
+        public string Name { get { return name; } }
+
+        public Todo(int id, DateTime createdAt, string name, bool isComplete, int userId)
+        {
+            this.id = id;
+            this.createdAt = createdAt;
+            this.name = name;
+            this.isComplete = isComplete;
+            this.userId = userId;
+        }
+
+        public override string ToString()
+        {
+            string c = "";
+            c += isComplete ? "completed" : "uncompleted";
+            return "---This is todo element with id: " + id + " which was created at: " + createdAt + " for user: "+ userId + " has name: " + name + " and it is " + c;
+        }
+    }
+}
